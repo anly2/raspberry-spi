3e07f34048449e034fad26c905f1aa6401744553
Anko Anchev <anko.anchev@gmail.com>
2016-04-28 00:37:55 +0100

    Web: tweaks to REST script

--- web/rest.php
+++ web/rest.php
@@ -3,7 +3,18 @@
 class REST {
 	public static $ARGS = array();
 
-	public static function handle($uri_pattern, $action) {
+	public static function handle($uri_pattern, $action, $method="ALL") {
+		if ($method != "ALL" && !(is_array($method) && in_array("ALL", $method))) {
+			$m = $_SERVER['REQUEST_METHOD'];
+			if (is_array($method)) {
+				if (!in_array($m, $method))
+					return;
+			} else {
+				if ($m != $method)
+					return;
+			}
+		}
+
 		$pattern = $uri_pattern;
 
 		if (strpos($pattern, "/") !== 0 || strrpos($pattern, "/", -1) !== strlen($pattern)-1) {
@@ -15,7 +26,8 @@ class REST {
 		}
 
 		if (preg_match($pattern, $_REQUEST["_url"], $matches))
-			$action($matches);
+			if ($action($matches) === false)
+				exit;
 	}
 }
 
